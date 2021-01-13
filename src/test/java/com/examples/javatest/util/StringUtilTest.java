d0c037f5292c518d03c3ec6ad79809f4de976551
Alexis <omaroquendo221@gmail.com>
2021-01-12 20:08:08 -0500

    Uso de JUnit

--- src/test/java/com/examples/javatest/util/StringUtilTest.java
+++ src/test/java/com/examples/javatest/util/StringUtilTest.java
@@ -1,18 +1,17 @@
 package com.examples.javatest.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
-        assertEquals(StringUtil.repeat("Qué más pues?", 3), "Qué más pues?Qué más pues?Qué más pues?");
+    @Test
+    public void testRepeat(){
+        Assert.assertEquals(StringUtil.repeat("Qué más pues?", 3), "Qué más pues?Qué más pues?Qué más pues?");
 
-        assertEquals(StringUtil.repeat("Hola Mijo", 5), "Hola MijoHola MijoHola MijoHola MijoHola Mijo");
+        Assert.assertEquals(StringUtil.repeat("Hola Mijo", 5), "Hola MijoHola MijoHola MijoHola MijoHola Mijo");
 
-        assertEquals(StringUtil.repeat("Hola", 1), "Hola");
+        Assert.assertEquals(StringUtil.repeat("Hola", 1), "Hola");
 
     }
 
-    private static void assertEquals(String actual, String expected) {
-        if(!actual.equals(expected)){
-            throw new RuntimeException(actual + " No es igual a " + expected);
-        }
-    }
 }
