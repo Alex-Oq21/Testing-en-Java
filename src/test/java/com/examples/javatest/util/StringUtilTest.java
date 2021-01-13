c1dfb84343f58101be208594f523353e6827031a
Alexis <omaroquendo221@gmail.com>
2021-01-12 20:32:25 -0500

    Orden de pruebas

--- src/test/java/com/examples/javatest/util/StringUtilTest.java
+++ src/test/java/com/examples/javatest/util/StringUtilTest.java
@@ -5,13 +5,26 @@ import org.junit.Test;
 
 public class StringUtilTest {
     @Test
-    public void testRepeat(){
-        Assert.assertEquals(StringUtil.repeat("Qué más pues?", 3), "Qué más pues?Qué más pues?Qué más pues?");
-
-        Assert.assertEquals(StringUtil.repeat("Hola Mijo", 5), "Hola MijoHola MijoHola MijoHola MijoHola Mijo");
-
+    public void testRepeat_Once(){
         Assert.assertEquals(StringUtil.repeat("Hola", 1), "Hola");
+    }
 
+    @Test
+    public void testRepeat_Three_Times(){
+        Assert.assertEquals(StringUtil.repeat("Qué más pues?", 3), "Qué más pues?Qué más pues?Qué más pues?");
+    }
+    @Test
+    public void testRepeat_Five_Times(){
+        Assert.assertEquals(StringUtil.repeat("Hola Mijo", 5), "Hola MijoHola MijoHola MijoHola MijoHola Mijo");
+    }
+    @Test
+    public void testRepeat_Zero_Times(){
+        Assert.assertEquals("", StringUtil.repeat("Hola", 0));
+    }
+    //No se espera un resultado, sólo retornar una excepción
+    @Test(expected = IllegalArgumentException.class)
+    public void testRepeat_Negative_Times(){
+        StringUtil.repeat("Hola", -1);
     }
 
 }
