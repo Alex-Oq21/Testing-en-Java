b64e64ee3906530107b0e69fbb5aa3af22a18eef
Alexis <omaroquendo221@gmail.com>
2021-01-14 18:34:54 -0500

    Test con principios TDD

---
+++ src/main/java/com/examples/javatest/util/DateUtil.java
@@ -0,0 +1,11 @@
+package com.examples.javatest.util;
+
+public class DateUtil {
+    public static boolean isLeapYear(int year){
+        if ((year % 400 == 0 )||(year % 4 == 0 && year % 100 !=0)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
