1c9a36e1edcf218e9f4e05ea0dcb9719c1faee85
Alexis <omaroquendo221@gmail.com>
2021-01-14 19:50:37 -0500

    Test de descuento a los precios

--- src/test/java/com/examples/javatest/discounts/PriceCalculatorShould.java
+++ src/test/java/com/examples/javatest/discounts/PriceCalculatorShould.java
@@ -15,7 +15,15 @@ public class PriceCalculatorShould {
     public void total_is_the_sum_of_prices(){
         PriceCalculator calculator = new PriceCalculator();
         calculator.addPrice(10.2);
-        calculator.addPrice(15.5);
-        assertThat(calculator.getTotal(), CoreMatchers.is(25.7));
+        calculator.addPrice(17.5);
+        assertThat(calculator.getTotal(), CoreMatchers.is(27.7));
+    }
+    @Test
+    public void  apply_discount_to_prices(){
+        PriceCalculator calculator = new PriceCalculator();
+        calculator.addPrice(12.5);
+        calculator.addPrice(17.5);
+        calculator.setDiscount(50);
+        assertThat(calculator.getTotal(), CoreMatchers.is(15.0));
     }
 }
